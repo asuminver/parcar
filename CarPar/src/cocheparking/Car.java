45be9b4120c962aff7966c0f95445b372d3da0d9
CristianDAW <cristianelx24@gmail.com>
2021-02-21 13:52:29 +0100

    Corregido

---
+++ CarPar/src/cocheparking/Car.java
@@ -0,0 +1,98 @@
+package cocheparking;
+
+import java.util.Arrays;
+import java.awt.Color;
+/**
+ * Class that models objects of type "car"
+ * 
+ * @Java MOOC team, UC3M
+ * @2.0
+ */
+public class Car
+{
+    // Attributes of the class Car
+    static int numberWheels = 4;                // Number of wheels                           
+    
+    Garage theGarage=null;
+
+    
+    /**
+     * Constructor of a car specifying the garage object
+     */
+    public Car (Garage g){
+    	theGarage = g;
+    }
+    /**
+     * Method that is being always invoked when the program is running
+     * @throws InterruptedException 
+     */
+    public static void main(String[] args) throws InterruptedException{
+    	Garage g = new Garage();
+    	Car car = new Car(g);
+        car.accelerate();
+        slowDown();
+        moveDown();
+        accelerate();
+        g.printGarage();
+//        turnRight();
+//        turnHeadlightsOn();
+//        turnHeadlightsOff();
+    }    
+
+    /**
+     * Accelerate the car
+     * @throws InterruptedException 
+     */
+    public static void accelerate() throws InterruptedException{
+        String c = new String("*");
+        String space = new String(" ");
+    	for(int i=0; i<5; i++)
+        {
+        	System.out.print(c);
+        	c = space.concat(c);
+        	Thread.sleep(1000);
+        }
+    }
+    
+    /**
+     * Slow down the car
+     * @throws InterruptedException 
+     */
+    public static void slowDown() throws InterruptedException{
+    	String c = new String("     *");
+    	for(int i=0; i<5; i++)
+        {
+        	System.out.print(c);
+        	c = c.substring(1);
+        	Thread.sleep(1000);
+        }
+    	System.out.println();
+    }    
+    
+    /**
+     * Move the car downwards
+     * @throws InterruptedException 
+     */    
+    public static void moveDown() throws InterruptedException{
+    	String c = new String("\t\t\t\t  *");
+    	for(int i=0;i<5;i++)
+        {
+    		if(i!=4)
+    			System.out.println(c);
+    		else 
+    			System.out.print(c);
+        	Thread.sleep(1000);
+        }
+    }
+    
+}
+
+/*
+*ERRORES
+*Los métodos no están declarados como static en el main
+*Los Strings estaban mal escritos
+*Habia un "for" separado con comas(,) en vez de punto y coma(;)
+*Existían "imports" que no se usaban java.util.Arrays y java.awt.Color
+*el método car.printGarage() debe ser g.printGarage()
+*el "sout" de Garage estaba mal escrito
+*/
---
+++ CarPar/src/cocheparking/Garage.java
@@ -0,0 +1,9 @@
+package cocheparking;
+
+public class Garage{
+    
+    public void printGarage() 
+    {
+        System.out.print("|*]");
+    }
+}
